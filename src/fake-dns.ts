a1c446084d2cb7735b96286120d05c5755ebba1f
Mehdikhody <khodayari.khoram@gmail.com>
2024-10-06 19:58:00 +0330

    starting over

--- src/fake-dns.ts
+++
@@ -1,8 +0,0 @@
-import { z } from 'zod';
-
-export const FakeDNSObject = z.object({
-    ipPool: z.string(),
-    poolSize: z.number().optional(),
-});
-
-export type FakeDNSObject = z.infer<typeof FakeDNSObject>;
--- src/index.ts
+++
@@ -1,5 +0,0 @@
-export * from './config';
-export * from './log';
-export * from './api';
-export * from './dns';
-export * from './fake-dns';
