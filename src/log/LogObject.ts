446eaec85727623ec32bc47e0cecfcad789b4561
Mehdikhody <khodayari.khoram@gmail.com>
2024-10-06 20:09:10 +0330

    add log configuration

---
+++ src/log/LogObject.ts
@@ -0,0 +1,11 @@
+import { z } from 'zod';
+
+export const LogObject = z.object({
+    access: z.string().optional(),
+    error: z.string().optional(),
+    loglevel: z.enum(['debug', 'info', 'warning', 'error', 'none']).optional(),
+    dnsLog: z.boolean().optional(),
+    maskAddress: z.enum(['quarter', 'half', 'full']),
+});
+
+export type LogObject = z.infer<typeof LogObject>;
